15ca470aa96331f6b933b830e116aade15662180
takehilo <takehilo.kaneko@gmail.com>
2023-09-07 01:55:15 +0900

    Initial Commit

---
+++ GithubApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GithubApp
+//
+//  Created by takehiro.kaneko on 2023/09/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubApp/GithubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubAppApp.swift
+//  GithubApp
+//
+//  Created by takehiro.kaneko on 2023/09/07.
+//
+
+import SwiftUI
+
+@main
+struct GithubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
